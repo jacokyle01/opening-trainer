6ec176a589b832e9bbe855c043c3c85133e9e028
Kyle Jacobson <jacokyle01@gmail.com>
2023-11-03 12:20:41 -0500

    Clarify meaning by using `ply` instead of `move`

--- opening_tree.py
+++ opening_tree.py
@@ -1,14 +1,14 @@
 class OpeningTree: 
     
-    def __init__(self, move, color):
-        self.move = move
+    def __init__(self, ply, color):
+        self.ply = ply
         self.color = color
         self.parent = -1
         self.children = []
 
         
     def __str__(self):
-        return self.color + " played " + self.move
+        return self.color + " played " + self.ply
 
 # helper method to set parent-child relationship
 def link(tree1, tree2):
